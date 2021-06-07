4efe1c9f189ec29ea06f576c94d3cb198286d720
Pavel Macík <pavel.macik@gmail.com>
2021-06-07 23:17:05 +0200

    Fix PR comments
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- .github/actions/setup-podman/setup-podman-wrapper.sh
+++ .github/actions/setup-podman/setup-podman-wrapper.sh
@@ -3,8 +3,5 @@
 set -x
 
 mkdir -p $HOME/.config/containers
-sed -e "s,REGISTRY_PREFIX,${REGISTRY_PREFIX},g" ./.github/actions/setup-podman/registries.conf > $HOME/.config/containers/registries.conf
-
-ls -la $HOME/.config/containers/registries.conf
-cat $HOME/.config/containers/registries.conf
+sed -e "s,REGISTRY_PREFIX,${REGISTRY_PREFIX},g" ./.github/actions/setup-podman/registries_template.conf > $HOME/.config/containers/registries.conf
 cp -rvf ./.github/actions/setup-podman/podman ${GITHUB_WORKSPACE}/bin/podman
